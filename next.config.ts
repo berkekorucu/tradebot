5ea60b85ab4dbda543cd853696de83df7206ba6c
Berke Korucular <korucu.berke@gmail.com>
2025-04-11 21:45:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./src/app
    > Font file not found: Can't resolve './assets/fonts/SF-Pro-Display-Regular.woff2'
    > . Error source: Call Stack
    > 5
    >
    > Show 5 ignore-listed frame(s)

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,23 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: 'dist',
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|otf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'static/fonts',
+            publicPath: '/_next/static/fonts',
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
