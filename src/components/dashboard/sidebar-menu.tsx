0ef14b15efe33085e1276050a0bd8557d8f05260
Berke Korucular <korucu.berke@gmail.com>
2025-04-11 21:32:34 +0000

    içeriği olmayan sekmeleri sil, profile pages ecommerce messages users

--- src/components/dashboard/sidebar-menu.tsx
+++ src/components/dashboard/sidebar-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar";
-import { Home, BarChart, Settings, TrendingUp, List, LayoutDashboard, Calendar, User, File, ShoppingCart, PieChart, Mail, Users } from "lucide-react";
+import { Home, BarChart, Settings, TrendingUp, List, LayoutDashboard, Calendar } from "lucide-react";
 import React from "react";
 
 const SidebarMenuComponent: React.FC = () => {
@@ -37,36 +37,6 @@ const SidebarMenuComponent: React.FC = () => {
           <a href="/calendar"><Calendar /><span>Calendar</span></a>
         </SidebarMenuButton>
       </SidebarMenuItem>
-       <SidebarMenuItem>
-        <SidebarMenuButton asChild tooltip="Profile">
-          <a href="/profile"><User /><span>Profile</span></a>
-        </SidebarMenuButton>
-      </SidebarMenuItem>
-       <SidebarMenuItem>
-        <SidebarMenuButton asChild tooltip="Pages">
-          <a href="/pages"><File /><span>Pages</span></a>
-        </SidebarMenuButton>
-      </SidebarMenuItem>
-      <SidebarMenuItem>
-         <SidebarMenuButton asChild tooltip="eCommerce">
-            <a href="/ecommerce"><ShoppingCart /><span>eCommerce</span></a>
-         </SidebarMenuButton>
-      </SidebarMenuItem>
-      <SidebarMenuItem>
-         <SidebarMenuButton asChild tooltip="Analytics">
-            <a href="/analytics"><PieChart /><span>Analytics</span></a>
-         </SidebarMenuButton>
-      </SidebarMenuItem>
-       <SidebarMenuItem>
-          <SidebarMenuButton asChild tooltip="Messages">
-             <a href="/messages"><Mail /><span>Messages</span></a>
-          </SidebarMenuButton>
-       </SidebarMenuItem>
-       <SidebarMenuItem>
-          <SidebarMenuButton asChild tooltip="Users">
-             <a href="/users"><Users /><span>Users</span></a>
-          </SidebarMenuButton>
-       </SidebarMenuItem>
     </SidebarMenu>
   );
 };
